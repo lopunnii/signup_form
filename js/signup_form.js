21ce76f148425c141e8def3539939c66ec128234
Lopunnii <kimi7@xtra.co.nz>
2022-10-19 14:29:11 +0900

    Add validation for password matching

--- js/signup_form.js
+++ js/signup_form.js
@@ -1,30 +1,15 @@
-document.getElementById("pw2").onkeyup = matchPassword();
-document.getElementById("submit_button").onclick = wrong_pw_alert()
-
-
 function matchPassword() {
-    let pw1 = document.getElementById("pw1");
-    let pw2 = document.getElementById("pw2");
+    let pw1 = document.getElementById("pw1").value;
+    let pw2 = document.getElementById("pw2").value;
     if (pw1 != pw2){
-        document.getElementById('wrong_pw_alert').style.color = 'red';
-        document.getElementById('wrong_pw_alert').innerHTML
-          = '*Passwords do not match';
-        document.getElementById('submit_button').disabled = true;
-        document.getElementById('submit_button').style.opacity = (0.4);
+        document.getElementById("pw_alert").textContent = "*Passwords do not match";
+        document.getElementById("pw_alert").style.color = "red";
+        document.getElementById("submit_button").disabled = true;
+        document.getElementById("submit_button").style.opacity = (0.4);
     } else {
-        document.getElementById('wrong_pw_alert').style.color = 'green';
-        document.getElementById('wrong_pw_alert').innerHTML =
-            'Password matched';
-        document.getElementById('submit_button').disabled = false;
-        document.getElementById('submit_button').style.opacity = (1);
-    }
-}
-
-function wrong_pw_alert() {
-    if (document.getElementById('pw').value != "" &&
-    document.getElementById('pw2').value != "") {
-    alert("Your response is submitted");
-    } else {
-    alert("Please fill all the fields");
+        document.getElementById("pw_alert").textContent = "Password matched";
+        document.getElementById("pw_alert").style.color = "green";
+        document.getElementById("submit_button").disabled = false;
+        document.getElementById("submit_button").style.opacity = (1);
     }
 }
