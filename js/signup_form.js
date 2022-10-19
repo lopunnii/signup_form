7895076a6d8e92fa18d45d3f115de069651a5d78
Lopunnii <kimi7@xtra.co.nz>
2022-10-19 15:10:17 +0900

    Remove red outline on required inputs on load up

--- js/signup_form.js
+++ js/signup_form.js
@@ -4,11 +4,13 @@ function matchPassword() {
     if (pw1 != pw2){
         document.getElementById("pw_alert").textContent = "*Passwords do not match";
         document.getElementById("pw_alert").style.color = "red";
+        document.getElementById("pw2").style.border = "red solid 1px";
         document.getElementById("submit_button").disabled = true;
         document.getElementById("submit_button").style.opacity = (0.4);
     } else {
         document.getElementById("pw_alert").textContent = "Password matched";
         document.getElementById("pw_alert").style.color = "green";
+        document.getElementById("pw2").style.border = "cornflowerblue solid 1px";
         document.getElementById("submit_button").disabled = false;
         document.getElementById("submit_button").style.opacity = (1);
     }
